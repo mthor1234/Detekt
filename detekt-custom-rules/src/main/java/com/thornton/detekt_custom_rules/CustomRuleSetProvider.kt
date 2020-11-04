b9a46d32899f590e2003a25e9a47bef70321ed0a
MJ <Mitchell.j.thornton@gmail.com>
2020-11-03 20:39:01 -0800

    Added Custom RuleSetProvider

---
+++ detekt-custom-rules/src/main/java/com/thornton/detekt_custom_rules/CustomRuleSetProvider.kt
@@ -0,0 +1,12 @@
+package com.thornton.detekt_custom_rules
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+class CustomRuleSetProvider : RuleSetProvider {
+    override val ruleSetId: String = "detekt-custom-rules"
+
+    override fun instance(config: Config)
+            = RuleSet(ruleSetId, listOf())
+}
